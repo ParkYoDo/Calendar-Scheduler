7f81b1bfb780879549c4736b8775fb06733a8f91
ParkYoDo <scarletdemon@naver.com>
2023-03-24 04:05:03 +0900

    globalStyle 폴더명 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Calendar from './components/Calendar/Calendar';
-import { GlobalStyle } from './globalstyle/globalStyle';
+import { GlobalStyle } from './globalStyle/globalStyle';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
