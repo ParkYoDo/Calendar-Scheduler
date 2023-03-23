bf4154a45ed918c4b49c9374db0a03d42c564ef3
ParkYoDo <scarletdemon@naver.com>
2023-03-24 05:18:26 +0900

    netlify 배포

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Calendar from './components/Calendar/Calendar';
-import { GlobalStyle } from './globalStyle/globalStyle';
+// import { GlobalStyle } from './globalStyle/globalStyle';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <GlobalStyle />
+        {/* <GlobalStyle /> */}
         <Calendar />
       </PersistGate>
     </Provider>
