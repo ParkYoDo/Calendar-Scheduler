571426fe7f8b5cb04d61742a92feb211c6bf1e00
ParkYoDo <scarletdemon@naver.com>
2023-03-24 14:10:53 +0900

    globalStyle 수정

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -199,11 +199,6 @@ function Calendar() {
           {nextDates.map((date, i) => makeNextCalendar(date, i))}
         </S.Dates>
       </S.CalendarBlock>
-      {/* {schedules.map((a, i) => (
-        <h1>
-          {i}. {a.date}:{a.content}
-        </h1>
-      ))} */}
     </>
   );
 }
