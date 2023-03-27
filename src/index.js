0c9884f777d3b6df64f05002bbd56602b577065a
ParkYoDo <scarletdemon@naver.com>
2023-03-27 17:34:30 +0900

    globalstyle 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { GlobalStyle } from './globalstyle/globalStyle';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,6 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle />
         <App />
       </PersistGate>
     </Provider>
