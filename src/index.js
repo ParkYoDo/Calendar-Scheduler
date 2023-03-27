2e94906de1e0ae2811179993df3324bfc9adaf8b
ParkYoDo <scarletdemon@naver.com>
2023-03-27 17:25:40 +0900

    globalstyle 수정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Calendar from './components/Calendar/Calendar';
 import { GlobalStyle } from './GlobalStyle/GlobalStyle';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import App from './App';
 
 const persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +14,7 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <GlobalStyle />
-        <Calendar />
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
